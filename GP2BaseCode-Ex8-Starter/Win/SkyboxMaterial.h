21143f74b0f0a0b5b63e9c582967cfa82a09bec5
Conor Johnston <cjohns201@caledonian.ac.uk>
2014-12-11 19:01:55 +0000

    SkyBoxMaterial.h has been completed
    Changes to the Material.h need to be made however need to move onto the
    SkyBoxMaterial.cpp first.

--- GP2BaseCode-Ex8-Starter/Win/SkyboxMaterial.h
+++ GP2BaseCode-Ex8-Starter/Win/SkyboxMaterial.h
@@ -4,20 +4,20 @@
 
 class SkyBoxMaterial : public BaseMaterial{
 
+public:
+	SkyBoxMaterial();
+	~SkyBoxMaterial();
+	void destroy();
+	void bind();
+	void unbind();
 
+	void loadCubeTexture(const std::string& filenamePosZ, const std::string filenameNegZ, const std::string& filenamePosX, cosnt std::string& filenameNegX, const std::string& filenameNegX,
+		const std::string& filenamePosY, const std::string& filenameNegY);
+	GLunit getCubeTexture();
 
-
-
-
-
+private:
+	GLunit m_CubeTexture;
 
 };
-
-
-
-
-
-
-
 
 #endif
